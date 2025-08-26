3a25d45ba5062b885ac9ae0e63693d4841976a3d
Matheus Rodrigues <matheusuniversidade@hotmail.com>
2025-08-26 18:49:30 -0300

    initial partial setup for bundles

--- src/views/profile/components/bundles-panel/BundlesPanel.jsx
+++ src/views/profile/components/bundles-panel/BundlesPanel.jsx
@@ -1,8 +1,36 @@
+import { useState } from "react";
 import "./BundlesPanel.css";
 function BundlesPanel() {
 
+    const [isVisible, setIsVisible] = useState(false);
+    const [owned] = useState(true);
+
+    const handleExtrasVisible = () => {
+        setIsVisible(!isVisible);
+    };
+
     return (
-        <div></div>
+        <div className="bundle-display">
+
+            {/* The main will display the more privileged information (Title, item amount, price, thumbnail and how many sold (optional) */}
+            <div className="main-bundle">
+
+                <sl-avatar image="#"></sl-avatar>
+                <h2 className="bundle-title">Title</h2>
+
+                <small>item amount</small>
+
+                <sl-button variant="success" className="buy-button">BUY</sl-button>
+
+            </div>
+            <footer>
+                <p className="see-more" onClick={() => handleExtrasVisible()}>See More</p>
+            </footer>
+
+            {/* Here will display other important content, description, licenses, size, tags and release date */}
+            <div style={{display: {isVisible}}} className="bundle-extras"></div>
+
+        </div>
     );
 }
 
