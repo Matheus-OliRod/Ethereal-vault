561b9ca512cee05ced59181ec969895fa98b09c9
Matheus Rodrigues <matheusuniversidade@hotmail.com>
2025-09-01 18:09:05 -0300

    Started chat UI

--- src/views/chat/Chat.jsx
+++ src/views/chat/Chat.jsx
@@ -1,8 +1,30 @@
+import { useState } from "react";
+import ContactCard from "./contact-card/ContactCard";
+import "./Chat.css";
 
 function Chat() {
 
+    const [messages] = useState({}); // Message blocks
+
     return (
-        <></>
+        <div className="chat-panel">
+            <nav className="chat-contacts">
+                <ContactCard />
+                <ContactCard />
+            </nav>
+
+            <div className="chat">
+                <nav className="chat-nav">
+                    <sl-details summary="Contact 1"></sl-details>
+                </nav>
+
+                <div className="message-holder"></div>
+                <form className="chat-input">
+                    <sl-input pill filled placeholder="Enter message"></sl-input>
+                    <sl-button variant="primary" className="send-message-button">Send</sl-button>
+                </form>
+            </div>
+        </div>
     );
 }
 
---
+++ src/views/chat/contact-card/ContactCard.jsx
@@ -0,0 +1,13 @@
+
+import "./ContactCard.css";
+function ContactCard() {
+
+    return (
+        <div className="contact-card">
+            <sl-avatar></sl-avatar>
+            <h2 className="contact-username">Username</h2>
+        </div>
+    );
+}
+
+export default ContactCard;
